a417a549219d6427445a660bc866c10b51c16f47
rishabh verma <vermarishabh0987@gmail.com>
2020-01-17 15:30:07 +0530

    panel list icons added and animated

--- src/components/panel/index.js
+++ src/components/panel/index.js
@@ -10,16 +10,38 @@ function Panel(props) {
     >
       <div className="user">
         <div className="userPhotoName">
-          <div className="image"></div>
-          <p>Rahul</p>
+          {props.slide ? null : (
+            <React.Fragment>
+              <div className="image"></div>
+              <p>Rahul</p>
+            </React.Fragment>
+          )}
         </div>
-        <div className="icon" onClick={() => props.slider()}></div>
+        <div className="icon" onClick={() => props.slider()}>
+          <img src={require("../../asset/icons/close.svg")} alt="Close" />
+        </div>
       </div>
-      <div className="panelList">
-        <h5>File My FIR</h5>
-        <h5>See all Filled FIR</h5>
-        <h5>Settings</h5>
-        <h5>Inbox</h5>
+      <div
+        className="panelList"
+        style={props.slide ? { marginLeft: "1rem" } : null}
+      >
+        <div className="fileFIR fir">
+          <img src={require("../../asset/icons/FIR.svg")} alt="FIR" />
+
+          {props.slide ? null : <h5>File My FIR</h5>}
+        </div>
+        <div className="fileFIR fir">
+          <img src={require("../../asset/icons/seeFIR.svg")} alt="FIR" />
+          {props.slide ? null : <h5>See My Filed FIR</h5>}
+        </div>
+        <div className="fileFIR fir">
+          <img src={require("../../asset/icons/settings.svg")} alt="FIR" />
+          {props.slide ? null : <h5>Settings</h5>}
+        </div>
+        <div className="fileFIR fir">
+          <img src={require("../../asset/icons/inbox.svg")} alt="FIR" />
+          {props.slide ? null : <h5>Inbox</h5>}
+        </div>
       </div>
     </section>
   );
