ac109e22e80b1dd4198471aa1093480884c3c7cf
rishabh verma <vermarishabh0987@gmail.com>
2020-01-18 12:23:59 +0530

    summary basic done

--- src/components/main/summary/summary-index.js
+++ src/components/main/summary/summary-index.js
@@ -4,6 +4,25 @@ function Summary() {
   return (
     <div className="summary">
       <h3>SummaryScreen</h3>
+      <div className="data">
+        <div className="card">
+          <h5>Title</h5>
+          <p>Ea cillum anim ea officia ex cillum consectetur adipisicing do.</p>
+        </div>
+        <div className="card">
+          <h5>Title</h5>
+          <p>Ea cillum anim ea officia ex cillum consectetur adipisicing do.</p>
+        </div>
+        <div className="card">
+          <h5>Title</h5>
+          <p>Ea cillum anim ea officia ex cillum consectetur adipisicing do.</p>
+        </div>
+        <div className="card">
+          <h5>Title</h5>
+          <p>Ea cillum anim ea officia ex cillum consectetur adipisicing do.</p>
+        </div>
+      </div>
+      <button>DONE</button>
     </div>
   );
 }
