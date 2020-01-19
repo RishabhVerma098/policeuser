697de77c3137945e309f6fc6ac058169dc55ccef
rishabh verma <vermarishabh0987@gmail.com>
2020-01-20 00:44:50 +0530

    summary page donedesign 1

--- src/components/main/summary/summary-index.js
+++ src/components/main/summary/summary-index.js
@@ -2,21 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./summary-index.scss";
 import { connect } from "react-redux";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 function Summary(props) {
   console.log(props.data);
   return (
     <div className="summary">
-      <h3>SummaryScreen</h3>
-      <div className="data">
-        {props.data
-          ? props.data.map(i => (
-              <div className="card" key={i.title}>
-                <h5>{i.title}</h5>
-                <p>{i.description}</p>
-              </div>
-            ))
-          : null}
-      </div>
+      <h3>Summary</h3>
+      <SimpleBar className="data">
+        {props.data ? (
+          props.data.map(i => (
+            <div className="card" key={i.title}>
+              <h5>{i.title}</h5>
+              <p>{i.description}</p>
+            </div>
+          ))
+        ) : (
+          <div className="no-data">
+            <p>
+              No Data is collected as for now , intract with chatBoy to do so ..
+            </p>
+          </div>
+        )}
+      </SimpleBar>
       {props.data ? (
         <Link to="/app/steps">
           <button className="DONE">DONE</button>
