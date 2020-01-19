efc26c0a9f6cb19cc169b302544ad3b65b4977ec
rishabh verma <vermarishabh0987@gmail.com>
2020-01-20 00:15:39 +0530

    userInputBar design1 done

--- src/components/main/chat/userInputBar/userInputBar.js
+++ src/components/main/chat/userInputBar/userInputBar.js
@@ -3,14 +3,16 @@ import "./userInputBar.scss";
 
 class UserInputBar extends React.Component {
   state = {
-    text: "write or tap on microphone"
+    text: "Write or tap on microphone"
   };
 
   render() {
     return (
       <div className="userInput">
         <div className="bar">
-          <div className="microphone"></div>
+          <div className="microphone">
+            <img src={require("../../../../asset/voice-recorder.svg")}></img>
+          </div>
           <div className="enterText">
             <input
               type="text"
@@ -19,7 +21,9 @@ class UserInputBar extends React.Component {
               maxLength="50"
             />
           </div>
-          <div className="go"></div>
+          <div className="go">
+            <img src={require("../../../../asset/multimedia.png")}></img>
+          </div>
         </div>
       </div>
     );
