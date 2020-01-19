137ea9681ec5789ef4f8bd14f8bcdfdf963b40a3
rishabh verma <vermarishabh0987@gmail.com>
2020-01-20 01:21:24 +0530

    filed and settings done design1

--- src/components/seeMyFieldFIR/index.js
+++ src/components/seeMyFieldFIR/index.js
@@ -1,65 +1,74 @@
 import React from "react";
 import "./index.scss";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 function AllFirs() {
   return (
     <div className="allFir">
       <h2>My Firs</h2>
-      <div className="listOfFir">
+      <SimpleBar className="listOfFir">
         <div className="fir">
-          <div className="firImage">
-            <h3>MY FIR</h3>
-          </div>
+          <div className="firImage"></div>
           <div className="firData">
             <h4>Title</h4>
-            <p className="timeStamp">created At:timeStamp</p>
+            <p className="timeStamp">created At: 24 April 20 , 3:40 PM</p>
             <p className="description">
               Tempor culpa laborum id eiusmod. Consequat irure aliqua minim
               pariatur ad. Voluptate est irure officia cillum sunt laborum
               eiusmod et sit ullamco ad id elit. Deserunt proident quis do in
-              nostrud consectetur adipisicing dolor sit id amet. Tempor
-              exercitation cupidatat commodo laboris id commodo nulla aute esse
-              excepteur aliquip magna dolor labore. Voluptate id ullamco
-              excepteur in aliquip....
             </p>
           </div>
         </div>
         <div className="fir">
-          <div className="firImage">
-            <h3>MY FIR</h3>
-          </div>
+          <div className="firImage"></div>
           <div className="firData">
             <h4>Title</h4>
-            <p className="timeStamp">created At:timeStamp</p>
+            <p className="timeStamp">created At: 24 April 20 , 3:40 PM</p>
             <p className="description">
               Tempor culpa laborum id eiusmod. Consequat irure aliqua minim
               pariatur ad. Voluptate est irure officia cillum sunt laborum
               eiusmod et sit ullamco ad id elit. Deserunt proident quis do in
-              nostrud consectetur adipisicing dolor sit id amet. Tempor
-              exercitation cupidatat commodo laboris id commodo nulla aute esse
-              excepteur aliquip magna dolor labore. Voluptate id ullamco
-              excepteur in aliquip....
+            </p>
+          </div>
+        </div>
+
+        <div className="fir">
+          <div className="firImage"></div>
+          <div className="firData">
+            <h4>Title</h4>
+            <p className="timeStamp">created At: 24 April 20 , 3:40 PM</p>
+            <p className="description">
+              Tempor culpa laborum id eiusmod. Consequat irure aliqua minim
+              pariatur ad. Voluptate est irure officia cillum sunt laborum
+              eiusmod et sit ullamco ad id elit. Deserunt proident quis do in
             </p>
           </div>
         </div>
         <div className="fir">
-          <div className="firImage">
-            <h3>MY FIR</h3>
-          </div>
+          <div className="firImage"></div>
           <div className="firData">
             <h4>Title</h4>
-            <p className="timeStamp">created At:timeStamp</p>
+            <p className="timeStamp">created At: 24 April 20 , 3:40 PM</p>
             <p className="description">
               Tempor culpa laborum id eiusmod. Consequat irure aliqua minim
               pariatur ad. Voluptate est irure officia cillum sunt laborum
               eiusmod et sit ullamco ad id elit. Deserunt proident quis do in
-              nostrud consectetur adipisicing dolor sit id amet. Tempor
-              exercitation cupidatat commodo laboris id commodo nulla aute esse
-              excepteur aliquip magna dolor labore. Voluptate id ullamco
-              excepteur in aliquip....
             </p>
           </div>
         </div>
-      </div>
+        <div className="fir">
+          <div className="firImage"></div>
+          <div className="firData">
+            <h4>Title</h4>
+            <p className="timeStamp">created At: 24 April 20 , 3:40 PM</p>
+            <p className="description">
+              Tempor culpa laborum id eiusmod. Consequat irure aliqua minim
+              pariatur ad. Voluptate est irure officia cillum sunt laborum
+              eiusmod et sit ullamco ad id elit. Deserunt proident quis do in
+            </p>
+          </div>
+        </div>
+      </SimpleBar>
     </div>
   );
 }
